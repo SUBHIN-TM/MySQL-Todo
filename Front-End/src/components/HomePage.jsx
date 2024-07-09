273091e4d6581d63845e105358eefe9ce333a584
SUBHIN-TM <subhintm2018@gmail.com>
2024-07-09 17:49:14 +0530

    first commit

---
+++ Front-End/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage=()=>{
+    return(
+        <>
+        <h1>Home page</h1>
+        <div className="main">
+            <div style={{backgroundColor:'blue'}}>1</div>
+            <div style={{backgroundColor:'violet'}}>2</div>
+            <div style={{backgroundColor:'red'}}> 3</div>
+        </div>
+        </>
+    )
+
+}
+
+export default HomePage
